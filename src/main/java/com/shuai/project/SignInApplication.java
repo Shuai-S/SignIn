f69499e6c5ff4f622282750182c37537c0c306c7
Zhushuai <zhu.shuai@digitalcnzz.com>
2022-03-16 21:01:34 +0800

    add：增加百度贴吧签到

--- src/main/java/com/shuai/project/SignInApplication.java
+++ src/main/java/com/shuai/project/SignInApplication.java
@@ -7,7 +7,7 @@ import com.shuai.project.domain.baidu.BaiDuTieBa;
 public class SignInApplication {
 
     public static void main(String[] args) {
-        System.out.println(new XSpeed().login());
+//        System.out.println(new XSpeed().login());
         // 贴吧签到
         new BaiDuTieBa().run(args);
 
